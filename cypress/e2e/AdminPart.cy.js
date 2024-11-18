89279a74b7243e12b2c562616d30f3776ebb5cbd
aracelliQA <aracellieng@gmail.com>
2024-11-18 16:01:42 -0300

    Adding logs

--- cypress/e2e/AdminPart.cy.js
+++ cypress/e2e/AdminPart.cy.js
@@ -1,3 +1,13 @@
+/*
+The Admin part testing includes the follow functionalities:
+1.	Login;
+2.	Validate HomePage;
+3.	Create user;
+4.	List user;
+5.	Create products;
+6.	List products.
+*/
+
 import home from '../support/pages/Admin/home-class'
 import addUser from '../support/pages/Admin/addUser-class'
 import listUser from '../support/pages/Admin/listUser-class'
@@ -13,23 +23,28 @@ describe('ServeRest - Admin user', () => {
   
   it('Homepage', () => {
      home.validate();
+     cy.log('Homepage validated.');
   });
 
   it('Add users', () => {
     addUser.add();
     addUser.addExisting();
+    cy.log('Adding users validated.');
   });
 
   it('List users', () => {
     listUser.list();
+    cy.log('User list validated.');
   });
 
   it.only('Add products', () => {
      addProduct.add();
+     cy.log('Adding products validated.');
   });
 
   it.only('List products', () => {
     listProducts.list();
+    cy.log('Products list validated.');
   });
 
 })
